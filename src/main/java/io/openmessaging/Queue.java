cdb51548732afb344bac899c809a1105b9296225
kirito <250577914@qq.com>
2018-07-11 21:40:57 +0800

    64k缓冲；单线程异步刷盘

--- src/main/java/io/openmessaging/Queue.java
+++ src/main/java/io/openmessaging/Queue.java
@@ -3,7 +3,11 @@ package io.openmessaging;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicLong;
 
 /**
@@ -42,7 +46,7 @@ public class Queue {
     // 记录该块中第一个消息的起始消息编号
     private int queueIndexes[] = new int[size];
 
-    private static final byte FILL_BYTE = (byte)0;
+    private static final byte FILL_BYTE = (byte) 0;
 
     private long offset;
     private int queueIndex;
@@ -56,6 +60,11 @@ public class Queue {
      */
     private int queueLength = 0;
 
+    private static ExecutorService flushThread = Executors.newSingleThreadExecutor();
+
+    private static ByteBuffer flushBuffer = ByteBuffer.allocateDirect(64 * 1024);
+
+    private Future<Long> flushFuture;
 
     /**
      * put 由评测程序保证了 queue 级别的同步
@@ -63,8 +72,8 @@ public class Queue {
      * @param message
      */
     public void put(byte[] message) {
-        if(firstPut){
-            this.queueIndex=0;
+        if (firstPut) {
+            this.queueIndex = 0;
             firstPut = false;
         }
         // 缓冲区满，先落盘
@@ -72,57 +81,100 @@ public class Queue {
             // 落盘
             flush();
         }
-        if(message.length<SINGLE_MESSAGE_SIZE){
+        if (message.length < SINGLE_MESSAGE_SIZE) {
             byte[] newMessage = new byte[SINGLE_MESSAGE_SIZE];
-            for(int i=0;i<SINGLE_MESSAGE_SIZE;i++){
-                if(i<message.length){
+            for (int i = 0; i < SINGLE_MESSAGE_SIZE; i++) {
+                if (i < message.length) {
                     newMessage[i] = message[i];
-                }else{
+                } else {
                     newMessage[i] = FILL_BYTE;
                 }
             }
             message = newMessage;
         }
         buffer.put(message);
-        this.queueLength ++;
+        this.queueLength++;
     }
 
     private void flush() {
-        buffer.flip();
-        long writePosition = wrotePosition.getAndAdd(bufferSize);
-        this.offset = writePosition;
-        try {
-            channel.write(buffer, writePosition);
-        } catch (IOException e) {
-            e.printStackTrace();
+        if (flushFuture != null) {
+            try {
+                offsets[blockSize - 1] = flushFuture.get();
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            }
+            flushFuture = null;
         }
+
+        buffer.flip();
+        int remaining = buffer.remaining();
+        final byte[] message = new byte[remaining];
+        buffer.get(message);
+        flushFuture = flushThread.submit(new Callable<Long>() {
+            @Override
+            public Long call() throws Exception {
+                long writePosition = wrotePosition.getAndAdd(message.length);
+                try {
+                    if (flushBuffer.remaining() < message.length) {
+                        flushBuffer.flip();
+                        channel.write(flushBuffer);
+                        flushBuffer.clear();
+                    }
+                    flushBuffer.put(message);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return writePosition;
+            }
+        });
         buffer.clear();
 
-
-        offsets[blockSize] = this.offset;
         queueIndexes[blockSize] = this.queueIndex;
         blockSize++;
-        if(blockSize >offsets.length * 0.7){
-            offsets = copyOf(offsets, offsets.length*2);
-            queueIndexes = copyOf(queueIndexes, queueIndexes.length*2);
+        if (blockSize > offsets.length * 0.7) {
+            offsets = copyOf(offsets, offsets.length * 2);
+            queueIndexes = copyOf(queueIndexes, queueIndexes.length * 2);
         }
 
         this.queueIndex += BLOCK_SIZE;
     }
 
     private void flushForGet() {
+        if (flushFuture != null) {
+            try {
+                offsets[blockSize - 1] = flushFuture.get();
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            }
+            flushFuture = null;
+        }
+
         buffer.flip();
-        long writePosition = wrotePosition.getAndAdd(bufferSize);
-        this.offset = writePosition;
+        flushFuture = flushThread.submit(new Callable<Long>() {
+            @Override
+            public Long call() throws Exception {
+                long writePosition = wrotePosition.getAndAdd(buffer.remaining());
+                try {
+                    if (flushBuffer.position() > 0) {
+                        flushBuffer.flip();
+                        channel.write(flushBuffer);
+                        flushBuffer.clear();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                channel.write(buffer);
+                buffer.clear();
+                return writePosition;
+            }
+        });
+
         try {
-            channel.write(buffer, writePosition);
-        } catch (IOException e) {
+            offsets[blockSize] = flushFuture.get();
+        } catch (InterruptedException | ExecutionException e) {
             e.printStackTrace();
         }
-        buffer.clear();
-//        ((DirectBuffer) buffer).cleaner().clean();
 
-        offsets[blockSize] = this.offset;
         queueIndexes[blockSize] = this.queueIndex;
         blockSize++;
     }
@@ -136,23 +188,19 @@ public class Queue {
      */
     public synchronized Collection<byte[]> get(long offset, long num) {
         if (firstGet) {
-            synchronized (this) {
-                if (firstGet) {
-                    flushForGet();
-                    firstGet = false;
-                }
-            }
+            flushForGet();
+            firstGet = false;
         }
-        if (offset > queueLength -1) {
+        if (offset > queueLength - 1) {
             return DefaultQueueStoreImpl.EMPTY;
         }
         int startIndex = (int) offset;
-        int endIndex = Math.min(startIndex + (int) num - 1, queueLength-1);
+        int endIndex = Math.min(startIndex + (int) num - 1, queueLength - 1);
 
         List<byte[]> result = new ArrayList<>();
 
-        if(lastReadOffset==startIndex){
-            while (startIndex<=endIndex&&buffer.hasRemaining()){
+        if (lastReadOffset == startIndex) {
+            while (startIndex <= endIndex && buffer.hasRemaining()) {
                 startIndex++;
                 byte[] cacheMessage = new byte[SINGLE_MESSAGE_SIZE];
                 buffer.get(cacheMessage);
@@ -163,7 +211,7 @@ public class Queue {
             }
         }
         // 从 cache 中获取到了所有的消息
-        if(startIndex>endIndex){
+        if (startIndex > endIndex) {
             return result;
         }
 
@@ -186,7 +234,7 @@ public class Queue {
 //            }else{
 //                size = BLOCK_SIZE - blockStartIndex;
 //            }
-            int blockStartIndex = j *BLOCK_SIZE;
+            int blockStartIndex = j * BLOCK_SIZE;
 
 
             buffer.clear();
@@ -197,16 +245,16 @@ public class Queue {
             }
             buffer.flip();
             for (int i = 0; i < BLOCK_SIZE; i++) {
-                if(startIndex <= blockStartIndex+i && blockStartIndex+i <= endIndex){
+                if (startIndex <= blockStartIndex + i && blockStartIndex + i <= endIndex) {
                     byte[] bytes = new byte[SINGLE_MESSAGE_SIZE];
                     buffer.get(bytes);
                     // TODO
 //                    bytes = truncate(bytes);
                     result.add(bytes);
-                    this.lastReadOffset = blockStartIndex+i+1;
-                }else if(blockStartIndex + i > endIndex){
+                    this.lastReadOffset = blockStartIndex + i + 1;
+                } else if (blockStartIndex + i > endIndex) {
                     break;
-                }else {
+                } else {
                     // skip
                     byte[] bytes = new byte[SINGLE_MESSAGE_SIZE];
                     buffer.get(bytes);
@@ -223,6 +271,7 @@ public class Queue {
                 Math.min(original.length, newLength));
         return copy;
     }
+
     public static long[] copyOf(long[] original, int newLength) {
         long[] copy = new long[newLength];
         System.arraycopy(original, 0, copy, 0,
@@ -230,10 +279,10 @@ public class Queue {
         return copy;
     }
 
-    private byte[] truncate(byte[] message){
+    private byte[] truncate(byte[] message) {
         int realSize = 0;
-        for(int i=0;i<SINGLE_MESSAGE_SIZE;i++){
-            if(message[i]==FILL_BYTE){
+        for (int i = 0; i < SINGLE_MESSAGE_SIZE; i++) {
+            if (message[i] == FILL_BYTE) {
                 realSize = i;
                 break;
             }
